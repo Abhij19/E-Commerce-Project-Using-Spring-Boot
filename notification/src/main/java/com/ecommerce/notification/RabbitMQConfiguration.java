8fe8cb42d874035c69105ae4e99830478241087a
Abhijeet Jha <jhaabhijeet6201@gmail.com>
2026-08-07 21:22:21 +0530

    Creating a notification service which will consume order service events after order is placed

---
+++ notification/src/main/java/com/ecommerce/notification/RabbitMQConfiguration.java
@@ -0,0 +1,64 @@
+package com.ecommerce.notification;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfiguration {
+
+    @Value("${rabbitmq.queue.name}")
+    private String queueName;
+    @Value("${rabbitmq.exchange.name}")
+    private String exchangeName;
+    @Value("${rabbitmq.routing-key.key}")
+    private String routingKey;
+
+    @Bean
+    public Queue queue() {
+        return QueueBuilder.durable(queueName).build();
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return ExchangeBuilder.topicExchange(exchangeName).durable(true)
+                .build();
+    }
+
+    @Bean
+    public Binding binding() {
+        // Bind this queue to the exchange we made with routing key
+        return BindingBuilder.bind(queue()).to(exchange()).with(routingKey);
+    }
+
+    // Now what this is doing is this helps automatically declare queues exchanges and binding when the app starts.
+    //And when we have set auto startup to true this means like no manual registration is needed.
+    @Bean
+    public AmqpAdmin amqpAdmin(ConnectionFactory connectionFactory) {
+        RabbitAdmin rabbitAdmin = new RabbitAdmin(connectionFactory);
+        rabbitAdmin.setAutoStartup(true);
+        return rabbitAdmin;
+    }
+
+    // So this tells RabbitMQ to convert Java objects into JSON when sending messages and vice versa when receiving,
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    // So rabbit template over here is used to send messages to rabbit MQ and what are we doing, we are setting the
+    // message converter and setting the default exchange over here for rabbit template to work with
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter());
+        rabbitTemplate.setExchange(exchangeName);
+        return rabbitTemplate;
+    }
+}
