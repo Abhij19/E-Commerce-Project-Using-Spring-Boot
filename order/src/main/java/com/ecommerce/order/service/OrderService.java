bf91a031c23f4822c2dbf97f3ec7a2a00e649f49
Abhijeet Jha <jhaabhijeet6201@gmail.com>
2026-08-08 13:57:39 +0530

    Changing type from map key value pair to order event in order service

--- order/src/main/java/com/ecommerce/order/service/OrderService.java
+++ order/src/main/java/com/ecommerce/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.ecommerce.order.service;
 
+import com.ecommerce.order.dto.OrderCreatedEvent;
 import com.ecommerce.order.dto.OrderItemDTO;
 import com.ecommerce.order.dto.OrderResponse;
 import com.ecommerce.order.model.CartItem;
@@ -9,12 +10,14 @@ import com.ecommerce.order.model.OrderStatus;
 import com.ecommerce.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -23,6 +26,11 @@ public class OrderService {
     private final CartService cartService;
     private final OrderRepository orderRepository;
     private final RabbitTemplate  rabbitTemplate;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchangeName;
+    @Value("${rabbitmq.routing-key.key}")
+    private String routingKey;
 
     public Optional<OrderResponse> createOrder(String userId) {
         // Validate for cart items
@@ -56,12 +64,35 @@ public class OrderService {
             )).toList();
         order.setItems(orderItemList);
         Order savedOrder = orderRepository.save(order);
+
         // Clear the cart
         cartService.clearCart(userId);
-        rabbitTemplate.convertAndSend("order-exchange", "order-tracking",
-                Map.of("orderId", savedOrder.getId(),"status", "CREATED"));
+
+        // Publish order created event
+        OrderCreatedEvent event = new OrderCreatedEvent(
+                savedOrder.getId(),
+                savedOrder.getUserId(),
+                savedOrder.getStatus(),
+                mapToOrderItemDTOs(savedOrder.getItems()),
+                savedOrder.getTotalAmount(),
+                savedOrder.getCreatedAt()
+        );
+
+        rabbitTemplate.convertAndSend(exchangeName, routingKey, event);
 
         return Optional.of(mapToOrderResponse(savedOrder));
+    }
+
+    private List<OrderItemDTO> mapToOrderItemDTOs(List<OrderItem> items) {
+        return items.stream()
+                .map(item -> new OrderItemDTO(
+                        item.getId(),
+                        item.getProductId(),
+                        item.getQuantity(),
+                        item.getPrice(),
+                        item.getPrice().multiply(BigDecimal.valueOf(item.getQuantity()))
+                ))
+                .toList();
     }
 
     private OrderResponse mapToOrderResponse(Order savedOrder) {
@@ -69,12 +100,16 @@ public class OrderService {
                 savedOrder.getId(),
                 savedOrder.getTotalAmount(),
                 savedOrder.getStatus(),
-                savedOrder.getItems().stream().map(orderItem -> new OrderItemDTO(
-                        orderItem.getId(),
-                        orderItem.getPrice(),
-                        orderItem.getProductId(),
-                        orderItem.getQuantity()
-                )).toList(),
+                savedOrder.getItems().stream()
+                        .map(orderItem -> new OrderItemDTO(
+                                orderItem.getId(),
+                                orderItem.getProductId(),
+                                orderItem.getQuantity(),
+                                orderItem.getPrice(),
+                                orderItem.getPrice().multiply(
+                                        BigDecimal.valueOf(orderItem.getQuantity()))
+                        ))
+                        .toList(),
                 savedOrder.getCreatedAt()
         );
     }
