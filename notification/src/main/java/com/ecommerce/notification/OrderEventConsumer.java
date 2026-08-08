8cd7ceb417c2bd724df37ca28415491b6fcdb532
Abhijeet Jha <jhaabhijeet6201@gmail.com>
2026-08-08 13:58:32 +0530

    Changing type from map key value pair to order event in handle order event consumer in notification service

--- notification/src/main/java/com/ecommerce/notification/OrderEventConsumer.java
+++ notification/src/main/java/com/ecommerce/notification/OrderEventConsumer.java
@@ -1,19 +1,19 @@
 package com.ecommerce.notification;
 
+import com.ecommerce.notification.payload.OrderCreatedEvent;
+import com.ecommerce.notification.payload.OrderStatus;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
-
-import java.util.Map;
 
 @Service
 public class OrderEventConsumer {
 
     @RabbitListener(queues = "${rabbitmq.queue.name}")
-    public void handleOrderEvent(Map<String, Object> orderEvent) {
+    public void handleOrderEvent(OrderCreatedEvent orderEvent) {
         System.out.println("Order event received: " + orderEvent);
 
-        long OrderId = Long.parseLong(orderEvent.get("orderId").toString());
-        String orderStatus = orderEvent.get("status").toString();
+        long OrderId = orderEvent.getOrderId();
+        OrderStatus orderStatus = orderEvent.getStatus();
 
         System.out.println("Order ID: " + OrderId);
         System.out.println("Order Status received: " + orderStatus);
